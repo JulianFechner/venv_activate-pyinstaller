633a4faf136b0e3f96eea10ccf7441954f8df9d2
Julian Fechner <79017692+JulianFechner@users.noreply.github.com>
2021-03-06 20:13:03 +0100

    Delete venv_enter_1.0.0_eng.bat

--- venv_enter_1.0.0_eng.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-echo Please specify the name for the virtual enviroment that you would like to activate and enter:
-set /p venv=
-call "C:\path\to\%venv%\Scripts\activate.bat"
-cd C:\path\to\%venv%
-python -m idlelib.idle
