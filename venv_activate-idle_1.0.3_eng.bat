5e58acc35320144526fae775afe8549efbda5b3a
Julian Fechner <79017692+JulianFechner@users.noreply.github.com>
2021-03-09 02:59:44 +0100

    Update venv_activate-idle_1.0.3_eng.bat

--- venv_activate-idle_1.0.3_eng.bat
+++ venv_activate-idle_1.0.3_eng.bat
@@ -21,7 +21,7 @@
 # SOFTWARE.
 
 @echo off 
-echo Please specify the name for the virtual enviroment that you would like to activate and enter:
+echo Please specify the name for the virtual enviroment that you would like to activate:
 set /p venv=
 call "C:\Users\Julian Fechner\pyproj\%venv%\Scripts\activate.bat"
 cd C:\Users\Julian Fechner\pyproj\%venv%
