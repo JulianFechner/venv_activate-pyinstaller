4da6ca4ff8a040cfcd802a4115451d6bd380dea8
Julian Fechner <79017692+JulianFechner@users.noreply.github.com>
2021-03-09 02:43:04 +0100

    Update and rename venv_enter_1.0.2_eng.bat to venv_activate+idle_1.0.3_eng.bat

--- venv_enter_1.0.2_eng.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-echo Please specify the name for the virtual enviroment that you would like to activate and enter:
-set /p venv=
-call "C:\Users\Julian Fechner\pyproj\%venv%\Scripts\activate.bat"
-cd C:\Users\Julian Fechner\pyproj\%venv%
-echo pip install --upgrade pip
-echo pip install -r requirements.txt
-python -m idlelib.idle
